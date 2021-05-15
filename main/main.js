34cd477e7928976dc43c5e0e68e66318b2687b9f
SaidmuhammadAkromov <saidakromov2002@gmail.com>
2021-05-15 15:33:23 +0500

    finish with fetch

--- main/main.js
+++ main/main.js
@@ -4,39 +4,22 @@ function editCards(card, importElements) {
     card.lastElementChild.children[1].innerHTML = importElements.body
 }
 
-
-
-
-
-
-
-
-
-
 window.onload = function () {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', 'http://192.144.37.95:8080/api/articles?langId=1', false);
-
-    xhr.send();
-
-    if (xhr.status != 200) {
-        alert( xhr.status + ': ' + xhr.statusText );
-    } else {
-        function returnXhr() {
-            const xhrResponseText = JSON.parse(xhr.responseText)
-            return xhrResponseText
-        }
-    }
-
-    const xhrResponseText = returnXhr()
     const biologCard0 = document.getElementById('biologCard0')
     const geografiyaCard0 = document.getElementById('geografiyaCard0')
     const kimyoCard0 = document.getElementById('kimyoCard0')
 
-    editCards(biologCard0, xhrResponseText[0])
-    editCards(geografiyaCard0, xhrResponseText[1])
-    editCards(kimyoCard0, xhrResponseText[3])
 
-    console.log(xhrResponseText);
+    fetch('http://192.144.37.95:8080/api/articles?langId=1')
+        .then(response => {
+           return response.json()
+        })
+        .then(function returnFetch(importElements) {
+            const elements = importElements
+            // return elements;
+            editCards(biologCard0, elements[0])
+            editCards(geografiyaCard0, elements[1])
+            editCards(kimyoCard0, elements[3])
+            console.log(elements);
+        });
 }
