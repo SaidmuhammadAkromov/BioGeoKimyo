f998c5113b57f6d16d58662bc06fec4ac7c43a39
SaidmuhammadAkromov <saidakromov2002@gmail.com>
2021-05-15 13:06:54 +0500

    init main js

---
+++ main/main.js
@@ -0,0 +1,22 @@
+// 1. Создаём новый объект XMLHttpRequest
+var xhr = new XMLHttpRequest();
+
+// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+xhr.open('GET', 'http://192.144.37.95:8080/api/articles?langId=1', false);
+
+// 3. Отсылаем запрос
+xhr.send();
+
+// 4. Если код ответа сервера не 200, то это ошибка
+if (xhr.status != 200) {
+  // обработать ошибку
+  alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+} else {
+  // вывести результат
+  const xhrResponseText = JSON.parse(xhr.responseText)
+  console.log(xhrResponseText[0]); 
+}
+
+window.onload = function () {
+     
+}
