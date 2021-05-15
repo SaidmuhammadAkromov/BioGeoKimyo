4ffd8a445c119c1bb2bf85d4f632880a317698a6
SaidmuhammadAkromov <saidakromov2002@gmail.com>
2021-05-15 14:43:50 +0500

    finish cards edit

--- main/main.js
+++ main/main.js
@@ -1,22 +1,42 @@
-// 1. Создаём новый объект XMLHttpRequest
-var xhr = new XMLHttpRequest();
-
-// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-xhr.open('GET', 'http://192.144.37.95:8080/api/articles?langId=1', false);
-
-// 3. Отсылаем запрос
-xhr.send();
-
-// 4. Если код ответа сервера не 200, то это ошибка
-if (xhr.status != 200) {
-  // обработать ошибку
-  alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-} else {
-  // вывести результат
-  const xhrResponseText = JSON.parse(xhr.responseText)
-  console.log(xhrResponseText[0]); 
+function editCards(card, importElements) {
+    card.firstElementChild.firstElementChild.src = importElements.image
+    card.lastElementChild.firstElementChild.innerText = importElements.title
+    card.lastElementChild.children[1].innerHTML = importElements.body
 }
 
+
+
+
+
+
+
+
+
+
 window.onload = function () {
-     
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'http://192.144.37.95:8080/api/articles?langId=1', false);
+
+    xhr.send();
+
+    if (xhr.status != 200) {
+        alert( xhr.status + ': ' + xhr.statusText );
+    } else {
+        function returnXhr() {
+            const xhrResponseText = JSON.parse(xhr.responseText)
+            return xhrResponseText
+        }
+    }
+
+    const xhrResponseText = returnXhr()
+    const biologCard0 = document.getElementById('biologCard0')
+    const geografiyaCard0 = document.getElementById('geografiyaCard0')
+    const kimyoCard0 = document.getElementById('kimyoCard0')
+
+    editCards(biologCard0, xhrResponseText[0])
+    editCards(geografiyaCard0, xhrResponseText[1])
+    editCards(kimyoCard0, xhrResponseText[3])
+
+    console.log(xhrResponseText);
 }
